37d9d730139fa75538b7feb444c50ebc9f37e6d6
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-05-07 18:07:18 +0200

    Mise à jour des fichiers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Additionnez tous les nombres positifs d'un tableau et retournez le résultat
+// Additionnez tous les nombres positifs d'un tableau et retourner le résultat
 // Exemple : [1, -2, 4] = 5
 // Attention s'il n'y a que des nombres négatifs, retournez 0
 
